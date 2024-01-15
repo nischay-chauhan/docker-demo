22889f3848154b185966d88f4436128fc617e255
nischay-chauhan <masternischay@outlook.com>
2024-01-15 12:22:36 +0530

    intial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+const app = express()
+
+app.get('/', (req, res) =>{
+res.send('Hello World!')
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT , () => console.log(`Server started on port ${PORT}`))
